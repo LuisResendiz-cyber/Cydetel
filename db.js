b9f60fa221a07ea72fa2f15dd60fe1d15557eec5
LuisResendiz-cyber <74916604+LuisResendiz-cyber@users.noreply.github.com>
2022-03-31 22:04:43 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
 [{
+        "id": 1,
         "user": "Ricardo",
-        "password": "12345"
+        "passsword": "12345"
     },
     {
+        "id": 2,
         "user": "Luis",
-        "password": "12345"
+        "passsword": "12345"
     },
     {
+        "id": 3,
         "user": "Fernando",
-        "password": "12345"
+        "passsword": "12345"
     }
 ]
